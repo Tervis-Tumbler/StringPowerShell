d509e219fa8ef6e774c35f5ff5c28e0bd38a5e84
ChrisMagnuson <hourglasssand@gmail.com>
2016-11-01 13:34:39 -0400

    Move string functions from other modules to new module

---
+++ StringPowerShell.psm1
@@ -0,0 +1,18 @@
+﻿function Test-ShouldBeAlphaNumeric {
+    param(
+        $Name, 
+        $string
+    )
+    if ($string -notmatch "^[a-zA-Z0-9]+$") { 
+        throw "The $name should only contain alphanumeric characters." 
+    }
+    $true
+}
+
+Function ConvertTo-TitleCase {
+    param(
+        [Parameter(ValueFromPipeline)][String]$Input
+    )
+    (Get-Culture).TextInfo.ToTitleCase($Input.ToLower())
+}
+
